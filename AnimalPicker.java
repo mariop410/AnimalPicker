7b2acdc614c9d1ec1ea2fdf4c847292202cf2e9c
mariop410 <mpanagrosso410@gmail.com>
2026-02-19 19:32:57 -0500

    asks user if they want to see a dog or a cat then prints out a placeholder message depending on their choice

--- AnimalPicker.java
+++ AnimalPicker.java
@@ -1,3 +1,18 @@
-public class AnimalPicker {
+import java.util.Scanner;
 
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Would you like to see a dog or cat?");
+
+        String choice = scanner.nextLine();
+
+        if (choice.equalsIgnoreCase("dog")) {
+            System.err.println("DOG ASCII ART HERE");
+        }
+        else if (choice.equalsIgnoreCase("cat")) {
+            System.err.println("CAT ASCII ART HERE");
+        }
+    }
 }
