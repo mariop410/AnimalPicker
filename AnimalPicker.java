ed7514c81e47358b622769f3754753e90aa9fdaf
KennethPappertQU <Kenneth.Pappert@quinnipiac.edu>
2026-02-26 12:56:04 -0500

    new option added

---
+++ AnimalPicker.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AnimalPicker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Would you like to see a dog, a fish, or cat?");
+
+        String choice = scanner.nextLine();
+
+        if (choice.equalsIgnoreCase("dog")) {
+            System.err.println("DOG ASCII ART HERE");
+        }
+        else if (choice.equalsIgnoreCase("cat")) {
+            System.err.println("CAT ASCII ART HERE");
+        }
+        else if (choice.equals("fish")) {
+            System.out.println("       .\r\n" + //
+                                "      \":\"\r\n" + //
+                                "    ___:____     |\"\\/\"|\r\n" + //
+                                "  ,'        `.    \\  /\r\n" + //
+                                "  |  O        \\___/  |\r\n" + //
+                                "~^~^~^~^~^~^~^~^~^~^~^~^~\r\n" + //
+                                "");
+        }
+    }
+}
